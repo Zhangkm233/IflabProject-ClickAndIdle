2f26bdecb4a54c10817faf805db147fdd9d16cf4
3KkM <32219924+Zhangkm233@users.noreply.github.com>
2025-05-25 14:46:41 +0800

    做了一点远程怪 还没做好

--- Assets/Script/EnemyObject.cs
+++ Assets/Script/EnemyObject.cs
@@ -3,12 +3,29 @@ using UnityEngine;
 public class EnemyObject : MonoBehaviour
 {
     private double enemyHp = 50;
+    public enum EnemyType
+    {
+        Normal,
+        Archer,
+    }
+    public enum EnemyScale
+    {
+        Small,
+        Mid,
+        Big
+    }
+    private EnemyType enemyType = EnemyType.Normal;
+    private EnemyScale enemyScale = EnemyScale.Small;
     public Sprite smallEnemy;
     public Sprite midEnemy;
     public Sprite bigEnemy;
 
     public void SetEnemyHp(double hp) {
         enemyHp = hp;
+    }
+
+    public void SetType(EnemyType type) {
+        enemyType = type;
     }
 
     public void SetEnemySprite(string enemyType) {
