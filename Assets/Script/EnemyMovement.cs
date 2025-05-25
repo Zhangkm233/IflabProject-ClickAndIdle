eb315448a6664829b295c0b523edd2881a1b4478
GaplouelPew <104667989+GateauPew@users.noreply.github.com>
2025-05-25 18:45:40 +0800

    扣血

--- Assets/Script/EnemyMovement.cs
+++ Assets/Script/EnemyMovement.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using System;
 
 public class EnemyMovement : MonoBehaviour
 {
     private float speed;
     private Vector2 moveDirection = Vector2.down;
+    private GameObject[] cannons;
 
-    public void SetSpeed(float newSpeed) {
+    public void SetSpeed(float newSpeed)
+    {
         speed = newSpeed;
     }
 
@@ -14,9 +17,20 @@ public class EnemyMovement : MonoBehaviour
         transform.Translate(moveDirection * speed * Time.deltaTime);
 
         // 如果移出屏幕底部，销毁对象
-        if (transform.position.y < -Camera.main.orthographicSize - 1f) {
-            
+        if (transform.position.y < -Camera.main.orthographicSize - 1f)
+        {
             this.GetComponent<EnemyObject>().DestroySelf();
+
+            cannons = GameObject.FindGameObjectsWithTag("CannonObject");
+            
+            System.Random random = new System.Random();
+            GameObject randomCannon = cannons[random.Next(0, cannons.Length)];
+
+            float health = randomCannon.GetComponent<CannonController>().cannonHp;
+            if (health != null)
+            {
+                health -= random.Next(1, 10);
+            }
         }
     }
 }
